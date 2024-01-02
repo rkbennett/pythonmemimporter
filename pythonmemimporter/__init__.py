0b9b0b96a31bb77ff1f858244c5866559a6c611f
rkbennett <44292326+rkbennett@users.noreply.github.com>
2024-01-02 09:18:27 -0600

    Update __init__.py

--- pythonmemimporter/__init__.py
+++ pythonmemimporter/__init__.py
@@ -5,6 +5,10 @@ import pythonmemorymodule
 threedottwelve = (sys.version_info.major == 3 and sys.version_info.minor >= 12)
 
 class _FuncPtr(ctypes._CFuncPtr):
+    """
+    Description:
+        This class wraps the ctypes._CFuncPtr class, which allows us to force replication of the behavior from ctypes.PyDLL
+    """
     _flags_ = ctypes._FUNCFLAG_CDECL | ctypes._FUNCFLAG_PYTHONAPI
     _restype_ = ctypes.py_object
 
@@ -14,6 +18,17 @@ class _memimporter(object):
         self.module = ctypes.c_void_p()
                             
     def import_module(self, modname, pathname, initfuncname, findproc, spec):
+        """
+        Description:
+            This function returns an init function from a python C extensions, which can be used to load python pyd files directly from memory
+        Args:
+            modname: module/package name to load
+            pathname: file path of target module
+            initfuncname: name of the init function for the module (PyInit_<modulename> typically)
+            findproc: python function pointer for function which can be used to retrieve raw bytes of pyd
+            spec: valid spec for the pyd
+        Returns:
+            result of an module's init function"""
         hmem = pythonmemorymodule.MemoryModule(data=findproc(modname))
         initf = hmem.get_proc_addr(initfuncname)
         self.module = ctypes.cast(initf, _FuncPtr)
@@ -24,5 +39,12 @@ class _memimporter(object):
         return self.module()
 
     def dlopen(self, data):
+        """
+        Description:
+            This function returns the address of pythonmemorymodule object once it is loaded
+        Args:
+            data: raw bytes of module to load
+        Returns:
+            Handle to pythonmemorymodule object"""
         self.module = pythonmemorymodule.MemoryModule(data=data)
         return ctypes.addressof(self.module.pythonmemorymodule.contents)
